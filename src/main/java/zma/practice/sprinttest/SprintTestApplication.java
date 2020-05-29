ac7ac713b312785b36ca73f8d69aaa43830dedb4
zhangmingao <zhangmingao@91jinrong.com>
2020-05-29 17:08:27 +0800

    一个新SpringBoot项目

---
+++ src/main/java/zma/practice/sprinttest/SprintTestApplication.java
@@ -0,0 +1,13 @@
+package zma.practice.sprinttest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SprintTestApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(SprintTestApplication.class, args);
+  }
+
+}
---
+++ src/test/java/zma/practice/sprinttest/SprintTestApplicationTests.java
@@ -0,0 +1,13 @@
+package zma.practice.sprinttest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SprintTestApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
